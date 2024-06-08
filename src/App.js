09f24563b2e0d1a092adb006609ca568568810b6
mohasinSH <ismail4111969@gmail.com>
2024-06-08 17:33:56 +0530

    second-commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React JS
         </a>
       </header>
     </div>
@@ -23,3 +23,5 @@ function App() {
 }
 
 export default App;
+
+
